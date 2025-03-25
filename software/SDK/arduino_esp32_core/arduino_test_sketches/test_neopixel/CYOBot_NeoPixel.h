c1d8ed16e7598d5183db0abd7009748aa9111ede
mhpham23 <mhpham23@vt.edu>
2025-03-25 18:59:04 -0400

    Create CYOBot_NeoPixel.h

---
+++ software/SDK/arduino_esp32_core/arduino_test_sketches/test_neopixel/CYOBot_NeoPixel.h
@@ -0,0 +1,19 @@
+#ifndef CYOBOT_NEOPIXEL_H
+#define CYOBOT_NEOPIXEL_H
+
+#include <Adafruit_NeoPixel.h>
+#include <list>
+
+#define LED_COUNT 33
+#define MAX_CHAR_INDICES 25
+#define MAX_PIXEL_LIST 25
+
+
+
+
+
+
+
+
+
+#endif
